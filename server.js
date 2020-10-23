afacad4a9436fbb9a662eee1a3f3c3fd34181f3a
Roxana <roxana.c2007@gmail.com>
2020-10-23 20:52:34 +0100

    server update

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ client.connect(function () {
 
 
 
+  
   app.post("/messages", function (req, res) {
     const collection = db.collection("chat-server");
     const addMessage = req.body;
     addMessage.timeSent = new Date();
-    console.log(addMessage);
     !addMessage.from || !addMessage.text
       ? res.send(404)
       : collection.insertOne(addMessage, function (error, result) {
@@ -45,50 +45,49 @@ client.connect(function () {
   app.get("/messages/search", function (req, res) {
     let searchText = { text: req.query.text };
     const collection = db.collection("chat-server");
-    client.connect(function () {
+  
       collection.findOne(searchText, function (error, messages) {
         res.send(error || messages);
-      });
+    
     });
   });
 
   app.get("/messages/latest", function (req, res) {
-    const collection = db.collection("chat-server");
-    client.connect(function () {
+    
       collection.find().toArray(function (error, messages) {
         res.send(error || messages.slice(-10));
-      });
+    
     });
   });
 
   app.get("/messages/:id", function (req, res) {
     let messageId = { _id: mongodb.ObjectId(req.params.id) };
     const collection = db.collection("chat-server");
-    client.connect(function () {
+   
       collection.findOne(messageId, function (error, messages) {
         res.send(error || messages);
       });
-    });
+ 
   });
 
   app.put("/messages/:id", function (req, res) {
     let messageId = { _id: mongodb.ObjectId(req.params.id) };
     const collection = db.collection("chat-server");
-    client.connect(function () {
+   
       collection.findOneAndUpdate(messageId, function (error, messages) {
         res.send(error || messages);
       });
-    });
+ 
   });
 
   app.delete("/messages/:id", function (req, res) {
     let messageId = { _id: req.params.id };
     const collection = db.collection("chat-server");
-    client.connect(function () {
+   
       collection.findOne(messageId, function (error, messages) {
         res.send(error || messages);
       });
-    });
+    
   });
 
   let port = process.env.PORT;
